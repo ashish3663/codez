adbf5b6d9602eee53c4e13c5705d6f6f335d8156
ashish3663 <160733491+ashish3663@users.noreply.github.com>
2024-02-21 17:31:58 +0530

    Uncommented code with indentation

--- sample.py
+++ sample.py
@@ -1,13 +1,14 @@
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route('/login')
-def login():
-    username = 'example_username'
-    password = 'example_password'
-ip = 'example_ip'
-port = 'example_port'
-
-    requests.post(ip + port, data={'username': username, 'password': password})
-    return 'Login request sent'
+    from flask import Flask, request
+    
+    app = Flask(__name__)
+    
+    @app.route('/login')
+    def login():
+        username = 'example_username'
+        password = 'example_password'
+    ip = 'example_ip'
+    port = 'example_port'
+    
+        requests.post(ip + port, data={'username': username, 'password': password})
+        return 'Login request sent'
+    
