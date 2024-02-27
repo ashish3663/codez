a071ef7c0db5fca8ca00866758e4395b092f62d6
ashish3663 <160733491+ashish3663@users.noreply.github.com>
2024-02-27 11:48:35 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 @app.route('/login')
 def login():
-    username = 'example_username'
-    password = 'example_password'
-    #ip = 'example_ip'  
-    #port = 'example_port' 
+    username = 'ashish'
+    password = 'ashish@123'
+    #ip = '10.0.0.1'  
+    #port = '9000' 
 
     requests.post(f'http://{ip}:{port}', data={'username': username, 'password': password})  # Send POST request
     return 'Login request sent'
