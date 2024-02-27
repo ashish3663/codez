318a42164f93f9c1b24c69ab543afca1af66c054
ashish3663 <160733491+ashish3663@users.noreply.github.com>
2024-02-27 16:40:13 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def login():
     username = 'ashish'
     password = 'ashish@123'
-    #ip = '10.0.0.1'  
+    ip = '10.0.0.1'  
     #port = '9000' 
 
     requests.post(f'http://{ip}:{port}', data={'username': username, 'password': password}) 
