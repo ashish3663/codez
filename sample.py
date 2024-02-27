03fe4dcc3871f8da2c18c977898dcc6d541b0f8c
ashish3663 <160733491+ashish3663@users.noreply.github.com>
2024-02-27 12:45:04 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -10,5 +10,5 @@ def login():
     #ip = '10.0.0.1'  
     #port = '9000' 
 
-    response = requests.post(f'http://{ip}:{port}', data={'username': username, 'password': password})  # Send POST request
+    result = requests.post(f'http://{ip}:{port}', data={'username': username, 'password': password})  # Send POST request
     return 'Login request sent'
