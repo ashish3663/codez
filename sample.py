53cebdc40e73fede75f4908b4da734e15d1b2586
ashish3663 <160733491+ashish3663@users.noreply.github.com>
2024-02-28 14:39:50 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -8,7 +8,7 @@ def login():
     username = 'ashish'
     password = 'ashish@123'
     #ip = '10.0.0.1'  
-    #port = '9000' 
+    port = '9000' 
 
     requests.post(f'http://{ip}:{port}', data={'username': username, 'password': password}) 
     return 'Login request sent'
